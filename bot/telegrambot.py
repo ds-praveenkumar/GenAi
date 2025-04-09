6948448fd62321ab47a0f74b7d2c1a7a40ce2b8a
Praveen Kumar <praveenkr2208@gmail.com>
2025-04-09 18:07:27 +0000

    update bot

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -17,10 +17,29 @@ def send_welcome(message):
 def send_info(message):
     bot.reply_to(message, "This is a simple Telegram bot implemented in Python.")
 
+@bot.message_handler(commands=["poll"])
+def create_poll(message):
+    bot.send_message(message.chat.id, "English Article Test")
+    answer_options = ["a", "an", "the", "-"]
+
+    bot.send_poll(
+        chat_id=message.chat.id,
+        question="We are going to '' park.",
+        options=answer_options,
+        type="quiz",
+        correct_option_id=2,
+        is_anonymous=False,
+    )
+
+@bot.poll_answer_handler()
+def handle_poll(poll):
+    # This handler can be used to log User answers and to send next poll
+    bot.send_message('received : ', poll)
+
 # Define a message handler
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
 # Start the bot
-bot.polling()
+bot.infinity_polling()
