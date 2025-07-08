875140268ff3b9ec51c3c7f590268775a8c99d77
ds-praveenkr <ds-praveenkr>
2025-07-08 23:00:11 +0530

    update tokenizer

--- slm/gpt2.py
+++ slm/gpt2.py
@@ -1,18 +1,23 @@
 
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, GPT2Config, GPT2LMHeadModel, pipeline
+import torch 
+torch.set_default_dtype(torch.float32) 
 
 class SLMGPT2:
     def __init__(self, tokenizer_id, model_id):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.dataset = load_dataset("tiny_shakespeare", trust_remote_code=True)
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_id)
+        self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
         self.distilled_model = AutoModelForCausalLM.from_pretrained(model_id)
-        self.vocab_size=self.tokenizer.vocab_size,
-        self.n_positions=128,
-        self.n_ctx=128,
-        self.n_embd=256,
-        self.n_layer=4,
+        self.vocab_size=self.tokenizer.vocab_size
+        self.n_positions=128
+        self.n_ctx=128
+        self.n_embd=256
+        self.n_layer=4
         self.n_head=4
+        print(f"device set: {self.device}")
         self.config = GPT2Config(
                         vocab_size=self.vocab_size,
                         n_positions=self.n_positions,
@@ -21,10 +26,20 @@ class SLMGPT2:
                         n_layer=self.n_layer,
                         n_head=self.n_head
                     )
-        self.model = GPT2LMHeadModel(self.config)
+        print(f"Vocab size: {self.config.vocab_size}, type: {type(self.config.vocab_size)}")
+        print(f"Embedding dim: {self.config.n_embd}, type: {type(self.config.n_embd)}")
+        self.model = GPT2LMHeadModel(self.config).to(device=self.device)
+        print(f"device set: {self.device}")
 
     def tokenize(self, example):
-        return self.tokenizer(example["text"], truncation=True, padding="max_length", max_length=128)
+        tokenized = self.tokenizer(
+            example["text"],
+            truncation=True,
+            padding="max_length",
+            max_length=128
+        )
+        tokenized["labels"] = tokenized["input_ids"].copy()
+        return tokenized
     
     def train(self):
         tokenized_dataset = self.dataset.map(self.tokenize, batched=True)
@@ -35,7 +50,6 @@ class SLMGPT2:
             per_device_eval_batch_size=4,
             logging_steps=10,
             save_steps=500,
-            evaluation_strategy="no",
             learning_rate=5e-4,
             weight_decay=0.01,
             logging_dir="none"
@@ -50,7 +64,6 @@ class SLMGPT2:
         print("training completed!!")
 
     def inference( self):
-        
         text_gen = self.pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
         print(text_gen("The universe is", max_length=50))
 
